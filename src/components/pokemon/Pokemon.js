273b386219a37a4aec1f2844945fd95049fe107a
Jordan Halfman <jordanhalfman@gmail.com>
2022-08-10 09:53:38 -0700

    updated json

--- src/components/pokemon/Pokemon.js
+++ src/components/pokemon/Pokemon.js
@@ -1,17 +1,8 @@
 import React from 'react';
 import { useEffect, useState } from 'react';
 
-const Pokemon = () => {
-    const [spriteList, setSpriteList] = useState([])
-
-    useEffect(() => {fetch("http://localhost:3000/pokemon")
-    .then(resp => resp.json())
-    .then(data => {
-        const sprites = data.map(mon => {
-        return <img src={mon.sprite} key={mon.name}></img>
-    })
-    setSpriteList(sprites);
-    })}, [])
+const Pokemon = ({spriteList}) => {
+    
 
     return (
         <div>{spriteList}</div>
--- src/components/static/App.js
+++ src/components/static/App.js
@@ -7,14 +7,23 @@ import Team from '../pokemon/Team';
 import Records from '../pokemon/Records';
 
 function App() {
+  const [spriteList, setSpriteList] = useState([])
 
+  useEffect(() => {fetch("http://localhost:3000/pokemon")
+  .then(resp => resp.json())
+  .then(data => {
+      const sprites = data.map(mon => {
+      return <img src={mon.sprite} key={mon.name}></img>
+  })
+  setSpriteList(sprites);
+  })}, [])
                 
   return (
     <div className="App">
       <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/pokemon" element={<Pokemon />} />
+        <Route path="/pokemon" element={<Pokemon spriteList={spriteList}/>} />
         <Route path="/team" element={<Team />} />
         <Route path="/records" element={<Records />} />
       </Routes>
