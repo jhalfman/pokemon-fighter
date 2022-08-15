096d84c76cce746d71da2b6d440a50066fee67eb
Jordan Halfman <jordanhalfman@gmail.com>
2022-08-15 08:41:23 -0700

    adding battle logic

--- src/components/pokemon/Battle.js
+++ src/components/pokemon/Battle.js
@@ -4,6 +4,8 @@ import { useLocation } from 'react-router-dom';
 const Battle = ({teamList, pokemonList}) => {
     const [currentTeam, setCurrentTeam] = useState({teamName: "default"});
     const [opponentTeam, setOpponentTeam] = useState(null);
+    const [currentFighter, setCurrentFighter] = useState(null);
+    const [opponentFighter, setOpponentFighter] = useState(null);
     
     const nameState = useLocation().state;
     useEffect(() => {
@@ -38,24 +40,101 @@ const Battle = ({teamList, pokemonList}) => {
         const opponentPokemon2 = pokemonList.filter(pokemon => pokemon.id === opponent2);
         const opponentPokemon3 = pokemonList.filter(pokemon => pokemon.id === opponent3);
         setOpponentTeam([...opponentPokemon1, ...opponentPokemon2, ...opponentPokemon3])
+    }
+
+    function opponentSelection() {
+        let randomOpp =  Math.floor(Math.random() * 3) + 1;
+        setOpponentFighter(opponentTeam[randomOpp]);
+    }
+
+    function attack(fighter, attackNumber) {
+        console.log(fighter.abilities[attackNumber])
+    }
+
+
+    let teamSelectionDiv;
+    if (currentTeam.teamName !== "default") {
+        teamSelectionDiv = (
+            <>
+                <select name="currentTeam" id="currentTeam" onChange={selectTeam} value={currentTeam.teamName}>
+                    <option key="default" value="default">SELECT A TEAM</option>
+                    {teamList.map(team => {
+                        return <option key={team.id} value={team.teamName}>{team.teamName}</option>
+                    })}
+                </select>
+                <img src={currentTeam.pokemon1.sprite}/>
+                <img src={currentTeam.pokemon2.sprite}/>
+                <img src={currentTeam.pokemon3.sprite}/>
+                <button onClick={startFight}>Start Fight</button>
+            </>
+        )
+    }
+    else {
+        teamSelectionDiv = (
+            <>
+                <select name="currentTeam" id="currentTeam" onChange={selectTeam} value={currentTeam.teamName}>
+                    <option key="default" value="default">SELECT A TEAM</option>
+                    {teamList.map(team => {
+                        return <option key={team.id} value={team.teamName}>{team.teamName}</option>
+                    })}
+                </select>
+            </>
+        )
+    }
         
+    
+    let battleDiv;
+    if (opponentTeam) {
+        const opponentTeamDisplay = opponentTeam.map(opponent => {
+            return (
+                <div key={opponent.id}>
+                    <h4>{opponent.name} - {opponent.alive ? (opponent.hp + "hp") : "Dead"}</h4>
+                    <img src={opponent.sprite}></img>
+                </div>
+            )
+        })
+        const userTeamDisplay = (
+            <>
+                <div onClick={() => {setCurrentFighter(currentTeam.pokemon1); opponentSelection()}}>
+                    <h4>{currentTeam.pokemon1.name} - {currentTeam.pokemon1.alive ? (currentTeam.pokemon1.hp + " hp"): "Dead"}</h4>
+                    <img src={currentTeam.pokemon1.sprite} />
+                </div>
+                <div onClick={() => {setCurrentFighter(currentTeam.pokemon2); opponentSelection()}}>
+                <h4>{currentTeam.pokemon2.name} - {currentTeam.pokemon2.alive ? (currentTeam.pokemon2.hp + " hp"): "Dead"}</h4>
+                    <img src={currentTeam.pokemon2.sprite} />
+                </div>
+                <div onClick={() => {setCurrentFighter(currentTeam.pokemon3); opponentSelection()}}>
+                <h4>{currentTeam.pokemon3.name} - {currentTeam.pokemon3.alive ? (currentTeam.pokemon3.hp + " hp"): "Dead"}</h4>
+                    <img src={currentTeam.pokemon3.sprite} />
+                </div>
+            </>
+        )
+        battleDiv = (
+            <>
+            {userTeamDisplay}
+            -----VS------
+            {opponentTeamDisplay}
+            {currentFighter ? (
+                <div>
+                    <img src={currentFighter.sprite}></img>
+                    <button onClick={() => attack(currentFighter, 0)}>{currentFighter.abilities[0]}</button>
+                    <button onClick={() => attack(currentFighter, 1)}>{currentFighter.abilities[1]}</button>
+                </div>
+            ) : null}
+            {opponentFighter ? (
+                <div>
+                    <img src={opponentFighter.sprite}></img>
+                </div>
+            ) : null}
+            </>
+        )
+            
     }
 
 
   return (
-    <div>
-        <select name="currentTeam" id="currentTeam" onChange={selectTeam} value={currentTeam.teamName}>
-            <option key="default" value="default">SELECT A TEAM</option>
-            {teamList.map(team => {
-                return <option key={team.id} value={team.teamName}>{team.teamName}</option>
-            })}
-        </select>
-        {currentTeam.teamName !== "default" ? <div><img src={currentTeam.pokemon1.sprite}/><img src={currentTeam.pokemon2.sprite}/> <img src={currentTeam.pokemon3.sprite}/></div> : null}
-        {currentTeam.teamName !== "default" ? <button onClick={startFight}>Start Fight</button> : null}
-        <h3>Opponent Team</h3>
-        {opponentTeam ? opponentTeam.map(opponent => {
-            return <img src={opponent.sprite}></img>
-        }) : null}
+    <div style={{display: 'flex'}}>
+        {battleDiv ? battleDiv : teamSelectionDiv}
     </div>
   )
 }
